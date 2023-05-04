3648f48b61b8d7178752fcf18586bc586ddc711b
Duane Northcutt <duane.northcutt@gmail.com>
2023-05-04 11:42:50 -0700

    Checkpoint.

--- ToneDetector/examples/ToneDetectorTest/ToneDetectorTest.ino
+++ ToneDetector/examples/ToneDetectorTest/ToneDetectorTest.ino
@@ -4,28 +4,79 @@
  * 
  ***************************************************************************/
 
+#include <I2S.h>
 #include <ToneDetector.h>
 
+//// TODO use logging
 
-#define FREQ						40
-#define NUM_SIDE_TONES	4
-#define NUM_SAMPLES			1024
+#define I2S_SAMPLE_RATE     16000
+#define BITS_PER_SAMPLE     16
+
+#define FREQ								43.1
+#define NUM_SAMPLES					128 // 1024
+//#define NUM_SAMPLES         (I2S_SAMPLE_RATE / 1)  // 1sec //// 10) 100msec
 
 
 ToneDetector *td;
 
+
+bool setupMic() {  //// FIXME take pins as parameters
+  I2S.setAllPins(-1, 42, 41, -1, -1);  //// FIXME
+  if (!I2S.begin(PDM_MONO_MODE, I2S_SAMPLE_RATE, BITS_PER_SAMPLE)) {
+    Serial.println("ERROR: Failed to initialize I2S!");
+    return(false);
+  }
+//	I2S.read();
+  return(true);
+}
+
+/*
+void readMic(int *samples, int numSamples) {
+	int read;
+	int available;
+	int toRead = numSamples;
+
+  while (toRead > 0) {
+  	available = I2S.available();
+    read = (available >= toRead) ? toRead : available;
+  	toRead -= I2S.read(samples, read);
+  }
+}
+*/
+void readMic(int *samples, int numSamples) {
+	for (int i = 0; (i < numSamples); i++) {
+		samples[i] = I2S.read();
+//		Serial.println(samples[i]);
+	}
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {;};
   Serial.println("ToneDetectorTest Starting...");
 
-  td = new ToneDetector(FREQ, NUM_SIDE_TONES, NUM_SAMPLES);
-  Serial.println("TDT: BEGIN");
+  if (setupMic()) {
+    Serial.println("INFO: Microphone ready");
+  } else {
+    while(1) {};    //// FIXME
+  }
+
+  td = new ToneDetector(FREQ);
+  Serial.println("ToneDetectorTest: BEGIN");
+}
+
+void printSamples(int samples[], int numSamples) {
+  Serial.print(">");
+  for (int i = 0; (i < numSamples); i++) {
+    Serial.print(String(samples[i]) + ", ");
+  }
+  Serial.println("");
 }
 
 void loop() {
-	Serial.println("LOOP");
-	td->detect();
+	int samples[NUM_SAMPLES];
+	readMic(samples, NUM_SAMPLES);
+//	printSamples(samples, NUM_SAMPLES);
+	td->detect(samples, NUM_SAMPLES);
 	delay(1000);
 }
