16c9ebfb6b81531f4210cce058967c2810867712
Duane Northcutt <duane.northcutt@gmail.com>
2023-08-24 09:39:04 -0700

    Converted tabs to spaces.

--- si1151/Si1151.h
+++ si1151/Si1151.h
@@ -5,30 +5,30 @@
 
 
 class Si1151 : public PollingComponent, public Sensor {
-	public:
-		Si115X sls;
-		Sensor *ir_sensor = new Sensor();
-		Sensor *vis_sensor = new Sensor();
-		Sensor *uv_sensor = new Sensor();
+  public:
+    Si115X sls;
+    Sensor *ir_sensor = new Sensor();
+    Sensor *vis_sensor = new Sensor();
+    Sensor *uv_sensor = new Sensor();
 
-		Si1151() : PollingComponent(60000) {};
+    Si1151() : PollingComponent(60000) {};
 
-		float get_setup_priority() const override {
-			return setup_priority::DATA;
-		};
+    float get_setup_priority() const override {
+      return setup_priority::DATA;
+    };
 
-		void setup() override { sls.Begin(); };
+    void setup() override { sls.Begin(); };
 
-		void update() override {
-			uint16_t ir = sls.ReadHalfWord();
-			ir_sensor->publish_state(ir);
-			uint16_t vis = sls.ReadHalfWord_VISIBLE();
-			vis_sensor->publish_state(vis);
-			float uv = sls.ReadHalfWord_UV();
-			uv_sensor->publish_state(uv);
-		};
-		/*
-		void setup() override {};
-		void update() override { publish_state(42.0); };
-		*/
+    void update() override {
+      uint16_t ir = sls.ReadHalfWord();
+      ir_sensor->publish_state(ir);
+      uint16_t vis = sls.ReadHalfWord_VISIBLE();
+      vis_sensor->publish_state(vis);
+      float uv = sls.ReadHalfWord_UV();
+      uv_sensor->publish_state(uv);
+    };
+    /*
+    void setup() override {};
+    void update() override { publish_state(42.0); };
+    */
 };
