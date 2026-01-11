3c07d7f818de943bbea7e2de78048d9333361df3
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-01-11 06:07:57 +0000

    Added args.

--- wyoming/bin/piper.sh
+++ wyoming/bin/piper.sh
@@ -3,7 +3,7 @@
 # Script to start the TTS container (optionally with gpu acceleration) for the
 #  Home Assistant Voice Assistant
 #
-# Usage: piper.sh {-n <voiceName>} {-p <hostPortNum>} {-g} {-v}
+# Usage: piper.sh {-n <voiceName>} {-p <hostPortNum>} {-d <dataPath>} {-g} {-v}
 
 PIPER_CPU="rhasspy/wyoming-piper"
 PIPER_GPU="ghcr.io/slackr31337/wyoming-piper-gpu:latest"
@@ -13,15 +13,18 @@ DEF_PIPER_VOICE="en_US-lessac-medium"
   # en_US-lessac-medium: English, medium-sized, ????
   # en_US-lessac-small: English, small-sized, ????
   # de_DE-jonas: German, male voice
+DEF_PIPER_DATA="${HOME}/wyoming/PiperData"
 
-PIPER_EXEC=$PIPER_CPU
-PIPER_PORT=$DEF_PIPER_PORT
-PIPER_VOICE=$DEF_PIPER_VOICE
+PIPER_EXEC=${PIPER_CPU}
+PIPER_PORT=${DEF_PIPER_PORT}
+PIPER_VOICE=${DEF_PIPER_VOICE}
+PIPER_DATA=${DEF_PIPER_DATA}
 
-while getopts "n:p:gv" opt; do
+while getopts "n:p:d:gv" opt; do
   case "$opt" in
     n) PIPER_VOICE="$OPTARG" ;;
     p) PIPER_PORT="$OPTARG" ;;
+    d) PIPER_DATA="$OPTARG" ;;
     g) PIPER_EXEC="--runtime=nvidia --gpus all $PIPER_GPU" ;;
     v) VERBOSE=1 ;;
     *) echo "Usage: $0 {-n <TTSvoiceName>} {-p <hostPortNum>} {-g} {-v}"; exit 1 ;;
@@ -32,14 +35,16 @@ if [ $VERBOSE ]; then
    echo "PIPER_VOICE: ${PIPER_VOICE}"
    echo "PIPER_PORT: ${PIPER_PORT}"
    echo "PIPER_EXEC: ${PIPER_EXEC}"
+   echo "PIPER_DATA: ${PIPER_DATA}"
 fi
 
 docker run -d --name wyoming-piper \
        -p ${PIPER_PORT}:10200 \
        --voice ${PIPER_VOICE} \
-       -v /home/jdn/Data2/PiperData:/data \
+       -v ${PIPER_DATA}:/data \
+       --restart unless-stopped \
        ${PIPER_EXEC}
 if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to start the Piper container for GPU execution"
+    echo "ERROR: Failed to start the Piper container"
     exit 1
 fi
